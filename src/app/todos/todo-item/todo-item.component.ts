85987b9d212afc2bb63b32a69c3bf8dc8c13268b
Cristian David Gutierrez <cdgutierrez456@gmail.com>
2022-06-22 20:10:21 -0500

    Enviando informacion a componente hijo (todo-item) Usando informacion e implementando dinamismo en campos input

--- src/app/todos/todo-item/todo-item.component.ts
+++ src/app/todos/todo-item/todo-item.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { Todo } from '../models/todo.model';
 
 @Component({
   selector: 'app-todo-item',
@@ -7,9 +9,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoItemComponent implements OnInit {
 
-  constructor() { }
+  @Input() todo: Todo = {
+    text: "",
+    id: 0,
+    completed: false
+  };
+  @ViewChild('inputFisico') txtInputFisico!: ElementRef;
+
+  checkCompleted: FormControl = new FormControl();
+  txtInput: FormControl = new FormControl();
+
+  editing: boolean = false;
+
+  constructor() {
+  }
 
   ngOnInit(): void {
+    this.checkCompleted = new FormControl(this.todo.completed);
+    this.txtInput = new FormControl(this.todo.text, [Validators.required])
+  }
+
+  edit() {
+    this.editing = true;
+    setTimeout(() => {
+      this.txtInputFisico.nativeElement.select();
+    }, 1);
+  }
+
+  onBlur() {
+    this.editing = false;
   }
 
 }
